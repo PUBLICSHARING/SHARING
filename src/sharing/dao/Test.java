9c9be170fff6938e25f4381033320132cdc6645e
YuanLi <yl123456789lxp@163.com>
2017-06-19 19:26:17 +0800

    First Commit

--- src/sharing/dao/Test.java
+++
@@ -1,7 +0,0 @@
-package sharing.dao;
-
-public class Test {
-	public static void main(String [] args){
-		System.out.println("hello world");
-	}
-}
--- src/sharing/entity/Test.java
+++
@@ -1,7 +0,0 @@
-package sharing.entity;
-
-public class Test {
-	public static void main(String [] args){
-		System.out.println("hello world");
-	}
-}
