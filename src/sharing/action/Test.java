5ac7bc1532186ca1c41adc7eb060baef2354e16b
LQ <2276264677@qq.com>
2017-06-19 17:17:44 +0800

    first

---
+++ src/sharing/action/Test.java
@@ -0,0 +1,7 @@
+package sharing.action;
+
+public class Test {
+	public static void main(String [] args){
+		System.out.println("hello world");
+	}
+}
---
+++ src/sharing/dao/Test.java
@@ -0,0 +1,7 @@
+package sharing.dao;
+
+public class Test {
+	public static void main(String [] args){
+		System.out.println("hello world");
+	}
+}
