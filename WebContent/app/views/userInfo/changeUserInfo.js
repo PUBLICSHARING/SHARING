eaf27bfc988f1c0c62b03141448411db0d07e63e
gaoxudong <2715196252@qq.com>
2017-07-02 20:29:19 +0800

    commit by gxd

--- WebContent/app/views/userInfo/changeUserInfo.js
+++ WebContent/app/views/userInfo/changeUserInfo.js
@@ -1,6 +1,7 @@
 var changeUserInfo = angular.module("changeUserInfo",[]);
 changeUserInfo.controller("changeUserInfoCtrl",["$scope","$rootScope","$state","$stateParams",function($scope,$rootScope,$state,$stateParams) {
-
+	$scope.user = {};
+	
 	$scope.submitInfo = function() {
 		alert("提交成功!");
 	}
