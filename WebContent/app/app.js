29b8c9a9a09886503087787a2ca9dec2c32fdb69
LQ <2276264677@qq.com>
2017-07-02 10:28:12 +0800

    LQapp.js

--- WebContent/app/app.js
+++ WebContent/app/app.js
@@ -1,4 +1,4 @@
-define(['angularAMD','angularUiRouter','angular','ngLoad','jquery','bootstrap','angularResource','dialog','angularFileUpload'],function (angularAMD,moment){
+define(['angularAMD','angularUiRouter','angular','ngLoad','jquery','bootstrap','angularResource','dialog','angularFileUpload','ngImgCrop'],function (angularAMD,moment){
 
     var app = angular.module('app',['ui.router','ngResource','dialogApp']);
 
@@ -56,6 +56,78 @@ define(['angularAMD','angularUiRouter','angular','ngLoad','jquery','bootstrap','
                     controllerUrl : ['../app/views/register/register.js',
                                      '../app/directives/foot/foot.js',
                                      '../app/service/user/userService.js']
+                })
+            }
+        }))
+                /*选择兴趣*/
+        .state('chooseInterests',angularAMD.route({
+            url : '/chooseInterests/:userId',
+            views : {
+                '' : angularAMD.route({
+                    templateUrl : '../app/views/chooseInterests/chooseInterests.html',
+                    controller : 'chooseCtrl',
+                    controllerUrl : ['../app/views/chooseInterests/chooseInterests.js',
+                                     '../app/directives/foot/foot.js',
+                                     '../app/service/user/userService.js']
+                })
+            }
+        }))
+        /*关于我们*/
+        .state('aboutUs',angularAMD.route({
+            url : '/aboutUs/:userId',
+            views : {
+                '' : angularAMD.route({
+                    templateUrl : '../app/views/aboutUs/aboutUs.html',
+                    controller : 'aboutCtrl',
+                    controllerUrl : ['../app/views/aboutUs/aboutUs.js',
+                                     '../app/directives/foot/foot.js',
+                                     '../app/service/user/userService.js']
+                })
+            }
+        }))
+        /*问题反馈*/
+        .state('question',angularAMD.route({
+            url : '/question/:userId',
+            views : {
+                '' : angularAMD.route({
+                    templateUrl : '../app/views/question/question.html',
+                    controller : 'questionCtrl',
+                    controllerUrl : ['../app/views/question/question.js',
+                                     '../app/directives/foot/foot.js',
+                                     '../app/service/user/userService.js']
+                })
+            }
+        }))
+        /*管理员登录*/
+        .state('adminLogin',angularAMD.route({
+            url : '/adminLogin',
+            views : {
+                '' : angularAMD.route({
+                    templateUrl : '../app/views/adminLogin/adminLogin.html',
+                    controller : 'alCtrl',
+                    controllerUrl : ['../app/views/adminLogin/adminLogin.js',
+                                     '../app/service/admin/adminService.js']
+                })
+            }
+        }))
+        /*管理系统*/
+        .state('adminMain',angularAMD.route({
+            url : '/adminMain',
+            templateUrl:'../app/views/adminMain/adminMain.html'
+        }))
+        /*管理员登录*/
+        .state('adminMain.index',angularAMD.route({
+            url : '/index/:adminId',
+            views : {
+                'left@adminMain' : angularAMD.route({
+                    templateUrl : '../app/views/adminMain/adminLeft/adminLeft.html',
+                    controller : 'adminLeftCtrl',
+                    controllerUrl : ['../app/views/adminMain/adminLeft/adminLeft.js']
+                }),
+                'right@adminMain' : angularAMD.route({
+                    templateUrl : '../app/views/adminMain/adminRight/adminRight.html',
+                    controller : 'adminRightCtrl',
+                    controllerUrl : ['../app/views/adminMain/adminRight/adminRight.js']
                 })
             }
         }))
