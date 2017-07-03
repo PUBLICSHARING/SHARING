3e8bd981a0cf7b086551aab35d431833f6b4fa29
gaoxudong <2715196252@qq.com>
2017-07-03 20:33:33 +0800

    commit by gxd

--- WebContent/app/views/userInfo/changeUserInfo.js
+++ WebContent/app/views/userInfo/changeUserInfo.js
@@ -1,17 +1,26 @@
 var changeUserInfo = angular.module("changeUserInfo",[]);
 changeUserInfo.controller("changeUserInfoCtrl",["$scope","$rootScope","$state","$stateParams","UserService",function($scope,$rootScope,$state,$stateParams,UserService) {
-	$scope.user = {};
+	$scope.init = function() {
+		$scope.user = {};
+		$scope.findUser();
+	}
 	
 	//方法,根据传递过来的用户ID展示用户信息
 	$scope.findUser = function() {
-		UserService
+		$scope.user.id = 26;
+		UserService.findUserById($scope.user.id, sucesscb, errorcb);
+		
+		function sucesscb(data) {
+			$scope.user = data;
+		}
+		
+		function errorcb(error) {
+			
+		}
 	}
 	
 	//方法,用于更新用户信息
 	$scope.submitInfo = function() {
-		$scope.user.id = 1;
-		$scope.user.password = "123456";
-		$scope.user.age = 20;
 		UserService.updateUser($scope.user, sucesscb, errorcb);
 		
 		function sucesscb(data) {
