89034d013bb871016d659ffe559f3705dd7aa914
gaoxudong <2715196252@qq.com>
2017-07-05 10:23:21 +0800

    commit by gxd

--- WebContent/app/directives/title/title.js
+++ WebContent/app/directives/title/title.js
@@ -2,11 +2,35 @@ var titleApp = angular.module("titleApp",[]);
 titleApp.directive("title",function() {
 	return{
 		scope:{
-			
+
 		},
 		restrict:'EMAC',
 		controller:['$scope','$rootScope','$state','$stateParams','UserService',function($scope,$rootScope,$state,$stateParams,UserService){
-			$scope.userId = $stateParams.userId;
+
+			$scope.init = function() {
+				$scope.userId = $stateParams.userId;
+				/*加载用户信息*/
+
+				UserService.findUserInfoTitleNeedById($scope.userId,sucesscb,errorcb);
+				function sucesscb(data) {
+					$scope.userInfo = data[""+ $scope.userId +""];
+				}
+				function errorcb(error) {
+					$rootScope.alertWarn("查询错误");
+				}
+
+				/*加载用户头像*/
+				UserService.findUserHeadImg($scope.userId, sucess, error);
+
+				function sucess(data) {
+					var b64 = data.replace('','data:image/png;base64,');
+					$scope.myCroppedImage = b64;
+				}
+
+				function error(error) {
+					$scope.myCroppedImage = "";
+				}
+			}
 			/*注册、登录跳转*/
 			$scope.go = function(state) {
 				$state.go(state);
@@ -15,30 +39,18 @@ titleApp.directive("title",function() {
 			$scope.goHome = function() {
 				$state.go("main.index",{userId:$scope.userId});
 			}
-			
-			/*加载用户信息*/
-			$scope.findUserInfoTitleNeedById = function() {
-				
-				UserService.findUserInfoTitleNeedById($scope.userId,sucesscb,errorcb);
-				function sucesscb(data) {
-					 $scope.userInfo = data[""+ $scope.userId +""];
-				}
-				function errorcb(error) {
-					$rootScope.alertWarn("查询错误");
-				}
-			}
-			
+
+
 			/*退出*/
 			$scope.exit = function() {
 				$scope.userId = null;
 			}
-			
+
 			/*跳转用户中心*/
 			$scope.manageUserInfo = function() {
 				$state.go("userInfo",{userId:$scope.userId});
 			}
-			
-			$scope.findUserInfoTitleNeedById();
+
 		}],
 		templateUrl:'../app/directives/title/title.html'
 	}
