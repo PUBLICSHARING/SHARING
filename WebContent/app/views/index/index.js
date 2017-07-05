3c738f4232ee0b6c2b008679729c064e6b5d11bd
LQ <2276264677@qq.com>
2017-07-05 21:27:17 +0800

    LQ

--- WebContent/app/views/index/index.js
+++ WebContent/app/views/index/index.js
@@ -30,4 +30,25 @@ indexApp.controller("indexCtrl",["$scope","$rootScope","$state","$stateParams","
 			
 		}
 	}
+	
+	
+	$scope.formatDate = function(longTime){
+		var date = new Date(longTime);
+		var year = date.getFullYear();
+	    var month = date.getMonth()+1;    //js从0开始取 
+	    var date1 = date.getDate(); 
+	    var hour = date.getHours(); 
+	    var minutes = date.getMinutes(); 
+	    var second = date.getSeconds();
+	    
+	    return "" + year + "年" + month + "月    " + hour + "时" + minutes + "分" + second + "秒";
+	}
+	
+	$scope.conment = function(index){
+		$("#" + index).slideDown(200);
+	}
+	
+	$scope.cancelConment = function(index){
+		$("#" + index).slideUp(200);
+	}
 }])
